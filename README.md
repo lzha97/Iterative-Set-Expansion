6ad0d9dbbc193dc4e054fcda30b649accbdd0d02
root <root@cs6111.c.cs6111-advanced-databases.internal>
2020-03-25 16:23:41 +0000

    kbp_annotator code, remove html comments with beautiful soup

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Iterative-Set-Expansion
 Project 2 for COMS6111 Advanced Databases
+
+### Dependencies: 
+- stanfordnlp 
+- stanford CoreNLP 
+- java 13 
+- bs4 (for beautiful soup)
--- iterative_set_expansion.py
+++ iterative_set_expansion.py
@@ -5,22 +5,49 @@ COMS6111 Advanced Databases
 25 March 2020 
 '''''''''''''''''''''
 
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Comment
 import sys 
 import requests
+import argparse 
+from stanfordnlp.server import CoreNLPClient
 
 
 ### Extract Plain Text From HTML
 def get_text_from_html(html): 
     soup = BeautifulSoup(html, 'html.parser')
     text = soup.find_all(text=True)
-    blacklist = ['[document]', 'noscript', 'header', 'html', 'meta', 'title', 'head', 'input', 'script', 'style']
+    for element in text: 
+        if isinstance(element, Comment): element.extract()
+    blacklist = ['form', 'button', 'svg', '[document]', 'noscript', 'header', 'html', 'meta', 'title', 'head', 'code', 'input', 'script', 'style']
     output = ''
-    for t in text: 
-        if t.parent.name not in blacklist: 
-            output += '{} '.format(t)
+    for t in text:
+        if t.parent:  
+            if t.parent.name not in blacklist: 
+                #print(t.parent.name)
+                output += '{} '.format(t)
     return output 
 
+def annotate_kbp(plain_text): 
+    parse = argparse.ArgumentParser()
+    parser.add_argument('--input_text_file_path', '-i', required=True, help="Path to the text file to be parsed")
+    args = parser.parse_args()
+
+    with open(args.input_text_file_path, 'r') as f: 
+        text = f.read() 
+
+    annotators_ner = ['tokenize', 'ssplit', 'pos', 'lemma', 'ner']
+    annotators_kbp = ['tokenize', 'ssplit', 'pos', 'lemma', 'ner', 'depparse', 'coref', 'kbp']
+
+    with CoreNLPClient(timeout=3000, memory = '4G') as pipeline:
+        for j in range(100): 
+            print(f'>>> Repeating {j}th time.')
+            ann_ner = pipeline.annotate(text, annotators = annotators_ner)
+            ann_kbp = pipeline.annotate(text, annotators = annotators_kbp)
+
+            for sentence in ann_kbp.sentence: 
+                for kbp_triple in sentence.kbpTriple: 
+                    print(f'\t Confidence: {kbp_triple.confidence};\t Subject: {kbp_triple.subject};\t Relation: {kbp_triple.relation}; Object: {kbp_triple.object}')
+    return 
 
 ### Get Input 
 G_API_KEY = sys.argv[1]
@@ -52,18 +79,19 @@ if response.status_code == 200:
     results = response.json()['items']
 
 
-webpages = dict()
+
 print('RESULTS:')
 for idx, val in enumerate(results): 
     print(str(idx+1)+':', val['title'])
+    print(val['link'])
     link = val['link']
     r = requests.get(link)
     if r.status_code == 200: 
         plain_text = get_text_from_html(r.content)[:20000]
-        webpages[idx] = plain_text
+        with open(str(idx)+'.txt', 'w+') as file: 
+            file.write(plain_text)
         print(len(plain_text))
         print()
+    
 
-
-#use webpages here: webpages = {0: some webpage, 1: some other webpage ..... 9: some last webpage}
-
+#webpages are saved by index in files <index>.txt
