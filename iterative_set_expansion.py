24107396b0aff79fbad072b19bd19ce2e390755c
root <root@cs6111.c.cs6111-advanced-databases.internal>
2020-03-25 15:41:35 +0000

    retrieve plain webpage with beautiful soup

--- iterative_set_expansion.py
+++ iterative_set_expansion.py
@@ -1,9 +1,28 @@
-import bs4 as bs 
+'''''''''''''''''''''
+Lillian Zha & Emily Hao
+Project 2
+COMS6111 Advanced Databases 
+25 March 2020 
+'''''''''''''''''''''
+
+from bs4 import BeautifulSoup
 import sys 
 import requests
 
-### Get Input 
 
+### Extract Plain Text From HTML
+def get_text_from_html(html): 
+    soup = BeautifulSoup(html, 'html.parser')
+    text = soup.find_all(text=True)
+    blacklist = ['[document]', 'noscript', 'header', 'html', 'meta', 'title', 'head', 'input', 'script', 'style']
+    output = ''
+    for t in text: 
+        if t.parent.name not in blacklist: 
+            output += '{} '.format(t)
+    return output 
+
+
+### Get Input 
 G_API_KEY = sys.argv[1]
 G_ENGINE_ID = sys.argv[2]
 RELATION = float(sys.argv[3]) # 1 for Schools_Attended, 2 for Work_For, 3 for Live_In, 4 for Top_Member_Employees
@@ -22,19 +41,29 @@ if not (K > 0 and K.is_integer()):
     sys.exit()
 
 
-### Build Request
 
+### Make Request to Custom Search API
 base_url = "https://www.googleapis.com/customsearch/v1"
 payload = {'key': G_API_KEY, 'cx': G_ENGINE_ID, 'q': QUERY, 'alt': 'json'}
 headers={'Accept': 'application/json'}
-
-
-### Make Request
-
+results = '' 
 response = requests.get(base_url, params=payload, headers=headers)
-results = response.json()['items']
+if response.status_code == 200: 
+    results = response.json()['items']
 
+
+webpages = dict()
 print('RESULTS:')
 for idx, val in enumerate(results): 
     print(str(idx+1)+':', val['title'])
+    link = val['link']
+    r = requests.get(link)
+    if r.status_code == 200: 
+        plain_text = get_text_from_html(r.content)[:20000]
+        webpages[idx] = plain_text
+        print(len(plain_text))
+        print()
+
+
+#use webpages here: webpages = {0: some webpage, 1: some other webpage ..... 9: some last webpage}
 
