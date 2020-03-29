ab8aca099344353504c27f8edb365eb992783579
Emily Hao <emilover426@yahoo.com>
2020-03-28 23:16:55 -0400

    full pipeline, rel and threshold check

--- example.py
+++ example.py
@@ -1,43 +1,40 @@
+from bs4 import BeautifulSoup, Comment
+import sys
+import requests
+import argparse
 from stanfordnlp.server import CoreNLPClient
 
-# example text
-print('---')
-print('input text')
-print('')
+def annotate_kbp(file_name,relation,threshold):
+    tuples = []
 
-text = "Chris Manning is a nice person. Chris wrote a simple sentence. He also gives oranges to people."
-text1 = "Joe Smith was born in Oregon."
-print(text1)
+    with open(file_name + ".txt", 'r') as f:
+        text = f.read()
 
-# set up the client
-print('---')
-print('starting up Java Stanford CoreNLP Server...')
+    annotators_ner = ['tokenize', 'ssplit', 'pos', 'lemma', 'ner']
+    annotators_kbp = ['tokenize', 'ssplit', 'pos', 'lemma', 'ner', 'depparse', 'coref', 'kbp']
 
-# set up the client
-#with CoreNLPClient(annotators=['tokenize','ssplit','pos','lemma','ner', 'parse', 'depparse','coref'], timeout=30000, memory='16G') as client:
-annotators_ner = ['tokenize', 'ssplit', 'pos', 'lemma', 'ner']
-annotators_kbp = ['tokenize', 'ssplit', 'pos', 'lemma', 'ner', 'depparse', 'coref', 'kbp']
-
-with CoreNLPClient(timeout=10000, memory = '4G',endpoint="http://localhost:9001") as pipeline:
-    for j in range(100):
-        #print(f">>>Repeating {j}th time.")
-        # submit the request to the server
-        """ann_ner = pipeline.annotate(text,annotators = annotators_ner)
+    with CoreNLPClient(timeout=10000, memory = '4G',be_quiet=True) as pipeline:
+        ann_ner = pipeline.annotate(text,annotators = annotators_ner)
         qualifying_sentences = []
         for s in ann_ner.sentence:
+            sentence_string = ""
             is_person = False
             for word in s.token:
                 if word.ner == "PERSON":
                     is_person = True
             if is_person == True:
-                qualifying_sentences.append(s)
-        #print("qualifying sentences: ")
-        #print(qualifying_sentences)"""
-        print("here")
-        ann_kbp = pipeline.annotate(text1, annotators = annotators_kbp)
-        print("after kbp")
-        for sentence in ann_kbp.sentence:
-            print("in here")
-            for kbp_triple in sentence.kbpTriple:
-                print("in here now")
-                print(f"\t Confidence: {kbp_triple.confidence};\t Subject: {kbp_triple.subject};\t Relation: {kbp_triple.relation}; Object: {kbp_triple.object}")
+                for word in s.token:
+                    sentence_string = sentence_string + " " + word.word
+            if sentence_string != "":
+                qualifying_sentences.append(sentence_string)
+
+        for sentence in qualifying_sentences:
+            ann_kbp = pipeline.annotate(sentence, annotators = annotators_kbp)
+            for sentence in ann_kbp.sentence:
+                for kbp_triple in sentence.kbpTriple:
+                    print(f"\t Confidence: {kbp_triple.confidence};\t Subject: {kbp_triple.subject};\t Relation: {kbp_triple.relation}; Object: {kbp_triple.object}")
+                    if kbp_triple.relation == relation and kbp_triple.confidence >= threshold:
+                        tuples.append((kbp_triple.subject,kbp_triple.relation,kbp_triple.object))
+    return tuples
+
+annotate_kbp("example","per:exmployee_or_member_of",0.7)
