43e67ce3c866a81df3be47c28b660323a9b3944e
Emily Hao <emilover426@yahoo.com>
2020-03-29 00:09:44 -0400

    iterate through all results

--- iterative_set_expansion.py
+++ iterative_set_expansion.py
@@ -113,7 +113,7 @@ for idx, val in enumerate(results):
             file.write(plain_text)
         print(len(plain_text))
         print()
-    if idx == 0:
+    if idx == 3:
         new_tuples = annotate_kbp(str(idx),RELATION,THRESHOLD)
         for tup in new_tuples:
             X.append(tup)
